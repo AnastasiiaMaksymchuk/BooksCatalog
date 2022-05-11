74d16fa8c72c5d0ef4a5c74d66c859ab4708d168
Анастасія <anastasiia.maksymchuk@oa.edu.ua>
2022-05-11 23:17:44 +0300

    Виправила

--- BooksCatalog/WebForm1.aspx.cs
+++ BooksCatalog/WebForm1.aspx.cs
@@ -31,28 +31,28 @@ namespace BooksCatalog
                     FileUpload1.SaveAs(Server.MapPath("~/ images /") + Label3.Text + ".jpg");
                     string bookimage = "images /" + Label3.Text + ".jpg";
                     String query = "insert into bookcatalog(bookid, bookname, authorname, publisher, pages, príce, bookimage) values(" + Label3.Text + ",’" + TextBox1.Text + "’,’" + TextBox2.Text + "’,’" + TextBox3.Text + "’," + TextBox4.Text + "," + TextBox5.Text + ",’" + bookimage + "’)";
-                          String mycon = "Data Source=DESKTOP - ATE5HJV; Initial Catalog=BookCatalog; Integrated Security = true";
+                    String mycon = "Data Source=DESKTOP - ATE5HJV; Initial Catalog=BookCatalog; Integrated Security = true";
                     SqlConnection con = new SqlConnection(mycon);
                     con.Open();
                     SqlCommand cmd = new SqlCommand();
                     cmd.CommandText = query;
                     cmd.Connection = con;
                     cmd.ExecuteNonQuery();
-                    Label4.Text = "Нова книга була додана успішно":
-getbookid();
-            TextBox1.Text = "";
-            TextBox2.Text = "";
-            TextBox3.Text = "";
-            TextBox4.Text = "";
-            TextBox5.Text = "";
-        }
-   else
-{ 
-Label4.Text = "Виберіть зображення";
-    }
+                    Label4.Text = "Нова книга була додана успішно";
+                    getbookid();
+                    TextBox1.Text = "";
+                    TextBox2.Text = "";
+                    TextBox3.Text = "";
+                    TextBox4.Text = "";
+                    TextBox5.Text = "";
+                }
+                else
+                {
+                    Label4.Text = "Виберіть зображення";
+                }
 
-}
-public void getbookid()
+            }
+            public void getbookid()
             {
                 String mycon = "Data Source=DESKTOP - ATE5HJV; Initial Catalog=BookCatalog; Integrated Security = True";
                 SqlConnection scon = new SqlConnection(mycon);
@@ -91,4 +91,4 @@ public void getbookid()
             }
 
         }
-    } 
+    } }
